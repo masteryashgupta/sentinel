f1d3cb2317bb6b5e4fe7d8b1a6ef88b1a9105574
yashgupta271 <yashgupta2711111@gmail.com>
2026-08-24 11:48:46 +0530

    feat: initialize React root with BrowserRouter and ToastProvider context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { ToastProvider } from "./context/ToastContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ToastProvider>
         <App />
       </ToastProvider>
